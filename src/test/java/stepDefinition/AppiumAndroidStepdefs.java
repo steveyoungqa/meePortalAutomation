77060cbeec8b9b710341edce8a967983aab30884
Steve Young <nocturnalsteve@gmail.com>
2016-09-27 16:03:33 +0100

    Updated Appium feature file to uses Data from Example tables

--- src/test/java/stepDefinition/AppiumAndroidStepdefs.java
+++ src/test/java/stepDefinition/AppiumAndroidStepdefs.java
@@ -1,58 +1,79 @@
 package stepDefinition;
 
+import cucumber.api.java.After;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.Select;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 
 public class AppiumAndroidStepdefs {
     AndroidDriver driver;
 
-    String avd;
-
-    //    AVD (Android Virtual Devices)
-    String avdGalaxyS7 = "Samsung_Galaxy_S7_Edge_API_22";
-    String avdNexus7 = "Nexus_7_API_22";
-    //    String avdNexus1 = "Nexus_One_API_24"; Nougat 7.0 Not supported by X-Code/Appium yet
-    String avdGalaxyTab4 = "Galaxy_Tab_4_0_API_23";
-
-    String apiVersion = "5.1";
-
-    String url = "https://mee-test-useraccesscontrolmanager.ws.macmillaneducation.com/";
-
-
-
-//    @Before
-//    public void setUp() throws MalformedURLException {
-//        DesiredCapabilities caps = new DesiredCapabilities();
-//        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, apiVersion);
-//        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-//        caps.setCapability(MobileCapabilityType.DEVICE_NAME,"Device");
-//        caps.setCapability("avd",avdNexus7);
-//        caps.setCapability(MobileCapabilityType.BROWSER_NAME, "Browser");
-//        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
-//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//    }
-
-
-    @Given("^I am using Appium to run automation tests for device \"([^\"]*)\"$")
-    public void iAmUsingAppiumToRunAutomationTestsForDevice(String device) throws Throwable {
-        driver.get(url);
-        WebElement registerButton = driver.findElement(By.xpath("//*[@data-reactid='.1.2.0.0']"));
-        registerButton.click();
-        WebElement firstName = driver.findElement(By.id("_FirstName"));
-        firstName.clear();
-        firstName.sendKeys("Arnold");
-        WebElement surname = driver.findElement(By.id("_LastName"));
-        surname.clear();
-        surname.sendKeys("Appium");
-        WebElement countrySelector = driver.findElement(By.xpath("//*[@data-flux-key='countryOfResidence']"));
-        Select country = new Select(countrySelector);
-        country.selectByValue("GB");
+    @Given("^I am using Appium to run automation tests for device \"([^\"]*)\" and Android version \"([^\"]*)\"$")
+    public void iAmUsingAppiumToRunAutomationTestsForDevice(String device, String version) throws Throwable {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, version);
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME,device);
+        caps.setCapability(MobileCapabilityType.BROWSER_NAME, "Browser");
+        caps.setCapability("avd",device);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
-//    @After
-//    public void tearDown() {driver.closeApp();}
+    @And("^I am using the \"([^\"]*)\" url for the mobile browser$")
+    public void iAmUsingTheUrlForTheMobileBrowser(String url) throws Throwable {
+        driver.get(url);
+    }
+
+    @Then("^I register a new user in the mobile browser in language \"([^\"]*)\"$")
+    public void iRegisterANewUserInTheMobileBrowser(String language) throws Throwable {
+        WebElement registerButton = driver.findElement(By.xpath("//*[@data-reactid='.1.2.0.0']"));
+        registerButton.click();
+//        WebElement languageSelector = driver.findElement(By.xpath("//*[@class='mee-select mee-purple-select']//*[@data-reactid = '.1.0']"));
+//        Select languageSelect = new Select(languageSelector);
+//        languageSelect.selectByValue(language);
+//  Selector not functioning on Android
+    }
+
+    @And("^I use a Country of \"([^\"]*)\" and a First Name of \"([^\"]*)\" and Surname of \"([^\"]*)\"$")
+    public void iUseALanguageOfFirstnameSurname(String country, String first, String last) throws Throwable {
+        WebElement countrySelector = driver.findElement(By.xpath("//*[@data-flux-key='countryOfResidence']"));
+        Select countrySelect = new Select(countrySelector);
+        countrySelect.selectByValue(country);
+        WebElement firstName = driver.findElement(By.id("_FirstName"));
+        firstName.clear();
+        firstName.sendKeys(first);
+        WebElement surname = driver.findElement(By.id("_LastName"));
+        surname.clear();
+        surname.sendKeys(last);
+    }
+
+
+    @Then("^use a date of birth of \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\"$")
+    public void useADateOfBirthOf(String day, String month, String year) throws Throwable {
+        WebElement daySelect = driver.findElement(By.xpath("//*[@id='_DateOfBirth_Day']"));
+        Select dayValue = new Select(daySelect);
+        dayValue.selectByValue(day);
+
+        WebElement monthSelect = driver.findElement(By.xpath("//*[@id='_DateOfBirth_Month']"));
+        Select monthValue = new Select(monthSelect);
+        monthValue.selectByValue(month);
+
+        WebElement yearSelect = driver.findElement(By.xpath("//*[@id='_DateOfBirth_Year']"));
+        Select yearValue = new Select(yearSelect);
+        yearValue.selectByValue(year);
+
+    }
+    @After
+    public void tearDown() {driver.closeApp();}
+
 }
--- src/test/resources/featureFiles/AppiumBrowser.feature
+++ src/test/resources/featureFiles/AppiumBrowser.feature
@@ -2,12 +2,13 @@
 Feature: Debug
 
   Scenario Outline: Registration Validation Errors Second Page
-    Given I am using Appium to run automation tests for device "<Device>"
-#    And I am using Android version "<Version>"
-#    And I have clicked on the Register button
-#    Then I enter a unique Gmail email address
-#    And I attempt to Paste confirmation of the unique Gmail email address
+    Given I am using Appium to run automation tests for device "<Device>" and Android version "<Version>"
+    And I am using the "<Url>" url for the mobile browser
+    Then I register a new user in the mobile browser in language "<Language>"
+    And I use a Country of "<Country>" and a First Name of "<Firstname>" and Surname of "<Surname>"
+    Then use a date of birth of "<day>" "<month>" "<year>"
+    Then I Pause for 5 seconds
 
     Examples:
-      | Device          |Version|
-      | Nexus_7_API_22  |5.1    |
+      | Device         | Version | Language | Firstname | Surname | Country | day | month | year | Url                                                                  |
+      | Nexus_7_API_22 | 5.1     | English  | Ted       | Tester  | GB      | 7   | 5     | 1956 | https://mee-test-useraccesscontrolmanager.ws.macmillaneducation.com/ |
